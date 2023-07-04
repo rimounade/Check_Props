98fa2e0550954f1d53bd951169bf2c2b2e40eb41
rimounade <rim.arfa1604@gmail.com>
2023-07-04 14:54:08 +0100

    first commit

---
+++ src/player.js
@@ -0,0 +1,8 @@
+const player = [
+    {name :'Cristiano Renaldo',age :'38', nationality:'Portuguese', team:"Nasr Saoudi",jerseyNumber:7,image : "https://upload.wikimedia.org/wikipedia/commons/2/23/Cristiano_Ronaldo_WC2022_-_01.jpg"},
+    {name : 'Leonel Messi',age:'36', nationality:'Argentine', team:"PSG", jerseyNumber:30, image : "https://upload.wikimedia.org/wikipedia/commons/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg"},
+    {name : 'Neimar', age:'31', nationality:'Brazilien', team:"PSG", jerseyNumber:10,image : "https://c8.alamy.com/compfr/p4ac44/sao-petersburgo-mo-21-06-2018-selecao-treina-em-sao-petersburgo-russie-saint-petersbourg-21-juin-2018-la-russie-coupe-du-monde-2018-la-formation-d-avant-match-et-la-conference-de-presse-de-l-equipe-nationale-bresilienne-dans-la-photo-joueur-de-l-equipe-nationale-du-bresil-neimar-durant-la-formation-photo-andrei-pronin-fotoarena-p4ac44.jpg"},
+    {name:'Mohamed Salah', age:'31', nationality:'Egyptian', team:"Liverpool", jerseyNumber:11,image :"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Mohamed_Salah_2018.jpg/280px-Mohamed_Salah_2018.jpg"}
+]
+
+export default player
